e96ff5ed27e9b19858f9faea478eba7cd114a245
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-02-27 14:13:22 +0100

    Initial commit for 9cloud

---
+++ 9cloud/weather/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='test'),
+    path('detail/<city_name>/', views.detail_city, name='test-detail_city'),
+    path('delete/<city_name>/', views.delete_city, name='delete_city'),
+]
