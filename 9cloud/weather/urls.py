245ee787a3e72096ab7c90b60febf5873d374191
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-05-04 17:18:25 +0200

    index function in views.py changed to CBV

--- 9cloud/weather/urls.py
+++ 9cloud/weather/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.HomeView.as_view(), name='index'),
     path('delete/<city_name>/', views.delete_city, name='delete_city'),
     path('detail/<city_name>/', views.detail_city, name='detail_city'),
 ]
--- 9cloud/weather/views.py
+++ 9cloud/weather/views.py
@@ -3,7 +3,53 @@ from .models import City
 from datetime import datetime
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.views.generic import ListView
+from django.views.generic.edit import FormMixin
 import requests
+
+
+class HomeView(ListView, FormMixin):
+    model = City
+    context_object_name = 'city'
+    template_name = 'weather/index.html'
+    form_class = CityForm
+
+    def get_queryset(self):
+        return City.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cities = City.objects.all().order_by('-id')
+        context['cities'] = cities
+        source = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=9a5f969d3828fdb7109c000f855bf43c'
+        weather_date = []
+        for city in cities:
+            data=requests.get(source.format(city)).json()
+            city_weather = {'city': city.name,
+                        'temperture': data['main']['temp'],
+                        'description': data['weather'][0]['description'],
+                        'icon': data['weather'][0]['icon']}
+            weather_date.append(city_weather)
+        context['weather_date'] = weather_date
+        return context
+
+    def post(self, request):
+        form = CityForm(request.POST)
+        source = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=9a5f969d3828fdb7109c000f855bf43c'
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                data = requests.get(source.format(new_city)).json()
+                if data['cod'] == 200:
+                    form.save()
+                    messages.success(self.request, f'City added successfully!')
+                else:
+                    messages.error(self.request, f'City does not exist in the world!')
+            else:
+                messages.warning(self.request, f'City already exists in the database!')
+        return redirect('index')
+
 
 
 def delete_city(request, city_name):
@@ -27,41 +73,3 @@ def detail_city(request, city_name):
     context = {'weather_data': weather_data, 'cities': cities}
 
     return render(request, 'weather/detail.html', context)
-
-
-def index(request):
-    cities = City.objects.all().order_by('-id')
-    source = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=9a5f969d3828fdb7109c000f855bf43c'
-    weather_date = []
-
-    if request.method == 'POST':
-        form = CityForm(request.POST)
-        if form.is_valid():
-            new_city = form.cleaned_data['name']
-            existing_city_count = City.objects.filter(name=new_city).count()
-            if existing_city_count == 0:
-                data = requests.get(source.format(new_city)).json()
-                if data['cod'] == 200:
-                    form.save()
-                    messages.success(request, f'City added successfully!')
-                else:
-                    messages.error(request, f'City does not exist in the world!')
-            else:
-                messages.warning(request, f'City already exists in the database!')
-
-    form = CityForm()
-
-    for city in cities:
-        data=requests.get(source.format(city)).json()
-        city_weather = {
-            'city': city.name,
-            'temperture': data['main']['temp'],
-            'description': data['weather'][0]['description'],
-            'icon': data['weather'][0]['icon']}
-        weather_date.append(city_weather)
-
-    context = {'cities': cities,
-            'form': form,
-            'weather_date': weather_date}
-
-    return render(request, 'weather/index.html', context)
